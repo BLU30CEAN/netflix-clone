2e04dd8bef12ab9805fc9c2b5a0125c670e51424
BLU30CEAN <ej.an.company@gmail.com>
2025-07-21 00:13:12 +0900

    Add Netflix Clone React app with GitHub Pages deployment

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Netflix Clone</h1>
+        <p>React로 만든 넷플릭스 클론 사이트입니다.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
