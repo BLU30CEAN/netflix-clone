3004c0b962eb6a48e8d2cd777a8da2e1fa2683a8
BLU30CEAN <ej.an.company@gmail.com>
2025-07-21 00:39:52 +0900

    Rename project to avoid security warnings

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Netflix Clone</h1>
-        <p>React로 만든 넷플릭스 클론 사이트입니다.</p>
+        <h1>Movie Streaming App</h1>
+        <p>React로 만든 영화 스트리밍 앱입니다.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
